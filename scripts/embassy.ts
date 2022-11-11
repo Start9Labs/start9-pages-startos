9249cf89aad90bf6440d73b6d9c3249679e174da
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-11-11 15:54:35 -0700

    convert main procedure to js

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,3 +3,4 @@ export { health } from "./procedures/health.ts";
 export { setConfig } from "./procedures/setConfig.ts";
 export { getConfig } from "./procedures/getConfig.ts";
 export { migration } from "./procedures/migrations.ts";
+export { main } from "./procedures/main.ts";
