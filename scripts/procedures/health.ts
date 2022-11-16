fdddf153af4b6089acf36924b35cf42241bbc77b
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-11-16 11:47:09 -0700

    update sdk and health check implementation to modern standard

--- scripts/procedures/health.ts
+++ scripts/procedures/health.ts
@@ -1,34 +1,9 @@
-import { types as T } from "../deps.ts";
+import { types as T, checkWebUrl, catchError } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
   // deno-lint-ignore require-await
   async "web-ui"(effects, duration) {
     // Checks that the server is running and reachable via http
-    return healthWeb(effects, duration);
+    return checkWebUrl("http://embassy-pages.embassy:80")(effects, duration).catch(catchError(effects))
   },
 };
-
-const healthWeb: T.ExpectedExports.health[""] = async (effects, duration) => {
-  await guardDurationAboveMinimum({ duration, minimumTime: 5000 });
-
-  return await effects.fetch("http://embassy-pages.embassy:80")
-    .then((_) => ok)
-    .catch((e) => {
-      effects.error(`${e}`)
-      return error(`The Embassy Pages UI is unreachable`)
-    });
-};
-
-// Ensure the starting duration is pass a minimum
-const guardDurationAboveMinimum = (
-  input: { duration: number; minimumTime: number },
-) =>
-  (input.duration <= input.minimumTime)
-    ? Promise.reject(errorCode(60, "Starting"))
-    : null;
-
-const errorCode = (code: number, error: string) => ({
-  "error-code": [code, error] as const,
-});
-const error = (error: string) => ({ error });
-const ok = { result: null };
