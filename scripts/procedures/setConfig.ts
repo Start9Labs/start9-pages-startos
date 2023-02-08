c5a30bdf51c7d2251a61740ceb543bd789064bce
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-02-07 20:24:51 -0700

    configure dependencies on config set

--- scripts/procedures/setConfig.ts
+++ scripts/procedures/setConfig.ts
@@ -1,7 +1,52 @@
 import { compat, types as T } from "../deps.ts";
+import { matchSubdomains, matchWebPageHomepage, matchWebPageSubdomain } from "../migrations/types.ts";
 export const setConfig: T.ExpectedExports.setConfig = async (
   effects,
   config,
 ) => {
-  return await compat.setConfig(effects, config, {});
+  let depFilebrowser: T.DependsOn = {}
+  let depGitea: T.DependsOn = {}
+  let depNextcloud: T.DependsOn = {}
+
+  if (matchWebPageHomepage.test(config)) {
+    switch (config.homepage.source) {
+      case 'filebrowser':
+        depFilebrowser =  { filebrowser: [] }
+        break;
+      case 'gitea':
+        depGitea =  { gitea: [] }
+        break;
+      case 'nextcloud':
+        depNextcloud =  { nextcloud: [] }
+        break;
+      default:
+        break;
+    }
+  }
+
+  if (matchSubdomains.test(config)) {
+    config.subdomains.map(sub => {
+      if (matchWebPageSubdomain.test(sub)) {
+        switch (sub.settings.source) {
+          case 'filebrowser':
+            depFilebrowser =  { filebrowser: [] }
+            break;
+          case 'gitea':
+            depGitea =  { gitea: [] }
+            break;
+          case 'nextcloud':
+            depNextcloud =  { nextcloud: [] }
+            break;
+          default:
+            break;
+        }
+      }
+    })
+  }
+
+  return await compat.setConfig(effects, config, {
+    ...depFilebrowser,
+    ...depGitea,
+    ...depNextcloud
+  });
 };
