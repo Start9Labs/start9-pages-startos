2c0bff22b04f34c47aece638779cb3b5106e04a4
Matt Hill <mattnine@protonmail.com>
2025-04-21 14:12:30 -0600

    copy

--- startos/main.ts
+++ startos/main.ts
@@ -116,8 +116,20 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
     })
   }
 
+  /**
+   * ======================== Additional Health Checks (optional) ========================
+   *
+   * In this section, we define *additional* health checks beyond those included with each daemon (below).
+   */
   const additionalChecks: T.HealthCheck[] = []
 
+  /**
+   * ======================== Daemons ========================
+   *
+   * In this section, we create one or more daemons that define the service runtime.
+   *
+   * Each daemon defines its own health check, which can optionally be exposed to the user.
+   */
   return sdk.Daemons.of(effects, started, additionalChecks).addDaemon(
     'primary',
     {
