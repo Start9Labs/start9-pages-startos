9ba09b3203f3c9c42e8e7c8590e8844b70a90fc5
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2025-05-22 17:41:13 -0400

    make create site task optional

--- startos/init/taskCreateSite.ts
+++ startos/init/taskCreateSite.ts
@@ -2,7 +2,7 @@ import { config } from '../actions/config'
 import { sdk } from '../sdk'
 
 export const taskCreateSite = sdk.setupOnInstall(async (effects) => {
-  await sdk.action.createOwnTask(effects, config, 'important', {
+  await sdk.action.createOwnTask(effects, config, 'optional', {
     reason: 'Create your first website!',
   })
 })
