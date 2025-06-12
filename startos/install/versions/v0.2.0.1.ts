229082e8ec9e5eaffd37b829cd1aa2421b5a3895
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2025-06-12 12:35:37 -0400

    update release notes

--- startos/install/versions/v0.2.0.1.ts
+++ startos/install/versions/v0.2.0.1.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs/promises'
 
 export const v0_2_0_1 = VersionInfo.of({
   version: '0.2.0:1',
-  releaseNotes: 'Revamped for StartOS 0.4.0',
+  releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {
       await fs
