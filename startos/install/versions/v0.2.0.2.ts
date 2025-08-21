6ad5b8dac7ca65ddfaff3e691e3101d4da193687
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2025-08-21 13:18:40 -0400

    bump version

--- startos/install/versions/v0.2.0.2.ts
+++ startos/install/versions/v0.2.0.2.ts
@@ -2,7 +2,7 @@ import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 import * as fs from 'fs/promises'
 
 export const v0_2_0_2 = VersionInfo.of({
-  version: '0.2.0:2-alpha.0',
+  version: '0.2.0:2-alpha.1',
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {
