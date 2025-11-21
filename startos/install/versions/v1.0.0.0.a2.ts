cad11e7ccff4288385c043af36925055d83c6cda
Mariusz Kogen <k0gen@pm.me>
2025-11-21 20:03:05 +0100

    sdk.beta.44 bump + devDeps update

--- startos/install/versions/v1.0.0.0.a2.ts
+++ startos/install/versions/v1.0.0.0.a2.ts
@@ -2,7 +2,7 @@ import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 import * as fs from 'fs/promises'
 
 export const v1_0_0_0_a2 = VersionInfo.of({
-  version: '1.0.0:0-alpha.2',
+  version: '1.0.0:0-alpha.3',
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {
